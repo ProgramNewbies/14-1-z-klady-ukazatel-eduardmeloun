ae717304cb8b35c53847726693c387c5ea488e6c
eduardmeloun <fejfaredik@gmail.com>
2026-04-22 15:28:42 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
-
-main()
+ 
+int main()
 {
-    int a;
-    int b;
-    int c;
-
-    printf("Zadej tri cisla:\n");
-
+    int a, b, c;
+ 
     scanf("%d", &a);
     scanf("%d", &b);
     scanf("%d", &c);
-
+ 
     int *p_a = &a;
     int *p_b = &b;
     int *p_c = &c;
-
+ 
     int soucet = *p_a + *p_b + *p_c;
-
+ 
     int max = *p_a;
+ 
     if (*p_b > max)
     {
         max = *p_b;
     }
-
+ 
     if (*p_c > max)
     {
         max = *p_c;
     }
-
-    printf("Soucet: %d \n", soucet);
-    printf("Maximum: %d\n", max);
-
+ 
+    printf("Soucet: %d\nMaximum: %d\n", soucet, max);
+ 
     if (soucet % 2 == 0)
     {
-        printf("Soucet je sudy");
+        printf("Soucet je sudy\n");
     }
     else
     {
-        printf("Soucet je lichy");
+        printf("Soucet je lichy\n");
     }
-
+ 
     return 0;
 }
+ 
